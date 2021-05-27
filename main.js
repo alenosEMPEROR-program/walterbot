d1b4730a428961078514404b1fba0c65cac31841
BuildTools <unconfigured@null.spigotmc.org>
2021-05-27 12:47:22 +0200

    hello

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 const discord = require("discord.js");
 const config = require("./config.json");
+
+function cmdFind(msg) {
+    return msg.slice(0, msg.indexOf(" "));
+}
+
 const client = new discord.Client();
+
 client.login(config.token);
+
+client.on("message", msg => {
+    if(!msg.content.startsWith(config.prefix)) return;
+    msg.content = msg.content(1);
+    switch(cmdFind(msg.content)) {
+        case "hello":
+            msg.channel.send("Hello to you 2 nerd...")
+            break;
+    }
+});
